1d9a8da0d3a39261fe5d8e7f8f813c52aca0fb61
JuanFrancoRodriguez <juanfrancorod@gmai.com>
2023-04-24 16:44:49 -0300

    mobile

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}", //esto también es una forma de englobar subdirectorios
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xsm: { min: "320px", max: "639px" },
+        sm1: { min: "640px", max: "1427px" },
+      },
+      backgroundImage: {
+        "fondo-main": "url('/src/img/7d0eea20d6c53007b24b7e2c4342b819 1.png')",
+        "fondo-mobile": "url('src/img/fondomobile.png')",
+      },
+    },
+    variants: {},
+    plugins: [],
+  },
+};
