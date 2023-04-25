df0bcfe6a5a75e424e4f7eb7e7c053e7710edaa6
JuanFrancoRod <127918947+JuanFrancoRod@users.noreply.github.com>
2023-04-25 15:19:06 -0300

    fixies (#4)
    Co-authored-by: JuanFrancoRodriguez <juanfrancorod@gmai.com>

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
-let apiUrl = 'http://localhost:8000/'
-if (process.env.NODE_ENV === 'production') {
-    apiUrl = process.env.NODE_ENV
+
+let apiUrl = "http://localhost:8000/";
+if (process.env.NODE_ENV === "production") {
+  apiUrl = process.env.NODE_ENV;
 }
-export default apiUrl
+export default apiUrl;
