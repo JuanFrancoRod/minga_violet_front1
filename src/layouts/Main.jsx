92348eeb772c3f5eb8cbe78d1b3ff6a11a861195
JuanFrancoRodriguez <juanfrancorod@gmai.com>
2023-04-26 13:11:33 -0300

    casifinal

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,16 +1,17 @@
 import React, { Children } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Mobile from '../components/Mobile';
 
 export default function Main(props) {
   return (
     <>
-      <div className="min-h-screen xsm:hidden">
+      <div className="min-h-screen ">
         <Navbar />
         {/* nav */}
         {props.children}
         {/*carrousel*/}
-        {/*main*/}
+              {/*main*/}
         <Footer />
         {/*footer*/}
       </div>
